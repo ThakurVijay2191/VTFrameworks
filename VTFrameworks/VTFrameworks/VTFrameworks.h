9accb9b279e2f6986288a3e00b9e715714d4b083
VijayNapworks <105584645+ThakurVijay2191@users.noreply.github.com>
2024-04-14 17:57:13 +0530

    Frameworks Initiated

---
+++ VTFrameworks/VTFrameworks/VTFrameworks.h
@@ -0,0 +1,18 @@
+//
+//  VTFrameworks.h
+//  VTFrameworks
+//
+//  Created by Nap Works on 14/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VTFrameworks.
+FOUNDATION_EXPORT double VTFrameworksVersionNumber;
+
+//! Project version string for VTFrameworks.
+FOUNDATION_EXPORT const unsigned char VTFrameworksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VTFrameworks/PublicHeader.h>
+
+
