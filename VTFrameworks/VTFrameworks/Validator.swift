9dfeb5aee57747cd6b81456c3d0fac46c3a5ee8a
VijayNapworks <105584645+ThakurVijay2191@users.noreply.github.com>
2024-04-14 18:32:24 +0530

    Second Commit

--- VTFrameworks/VTFrameworks/Validator.swift
+++ VTFrameworks/VTFrameworks/Validator.swift
@@ -8,20 +8,7 @@
 import Foundation
 
 public struct Validator {
-   public static func isValidEmail(_ email: String) -> Bool {
-        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-
-        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-        return emailPred.evaluate(with: email)
-    }
-    
     public static func sayHello(){
-        privateStruct.sayHello()
-    }
-}
-
-fileprivate struct privateStruct{
-    static func sayHello(){
-        print("This is hello from private function.")
+        print("Say Hello")
     }
 }
